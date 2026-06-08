3a8b6d367e09a684cb46e82558537b8e30f4889c
H4kErMaN <132083093+H4kErMaN@users.noreply.github.com>
2026-06-08 18:10:01 +0300

    Add files via upload

---
+++ fso2/puhelinluettelo/puhelinluettelostep11/App.jsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import Notification from './components/Notification'
+import personService from './services/persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const showNotification = (message) => {
+    setNotificationMessage(message)
+    setTimeout(() => {
+      setNotificationMessage(null)
+    }, 5000)
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    const existingPerson = persons.find(person => person.name === newName)
+
+    if (existingPerson) {
+      const confirmMessage =
+        `${newName} is already added to phonebook, replace the old number with a new one?`
+      if (window.confirm(confirmMessage)) {
+        personService
+          .update(existingPerson.id, { ...existingPerson, number: newNumber })
+          .then(updatedPerson => {
+            setPersons(persons.map(person =>
+              person.id !== existingPerson.id ? person : updatedPerson
+            ))
+            showNotification(`Updated ${updatedPerson.name}'s number`)
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+      return
+    }
+
+    personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        showNotification(`Added ${returnedPerson.name}`)
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  const deletePerson = (person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+        .remove(person.id)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== person.id))
+          showNotification(`Deleted ${person.name}`)
+        })
+    }
+  }
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Notification message={notificationMessage} />
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} deletePerson={deletePerson} />
+    </div>
+  )
+}
+
+export default App
