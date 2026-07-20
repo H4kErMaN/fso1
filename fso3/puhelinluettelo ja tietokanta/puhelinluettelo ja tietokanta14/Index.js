b2b2175098ed0958df55a78c981c8dd74f443fc0
H4kErMaN <132083093+H4kErMaN@users.noreply.github.com>
2026-07-21 01:05:56 +0300

    Create Index.js

---
+++ fso3/puhelinluettelo ja tietokanta/puhelinluettelo ja tietokanta14/Index.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
+app.use(morgan('tiny'))
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+app.get('/info', (request, response) => {
+  Person.countDocuments({}).then(count => {
+    const requestTime = new Date()
+    response.send(
+      `<p>Phonebook has info for ${count} people</p>
+       <p>${requestTime}</p>`
+    )
+  })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ error: 'name is missing' })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ error: 'number is missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
